3382757fbb815deac22bd03f894945e4220f5f42
edward519 <byeongjin258@naver.com>
2019-11-12 16:03:53 +0900

    edit ch3_ex1.cpp

--- ch3/ch3_ex1.cpp
+++ ch3/ch3_ex1.cpp
@@ -3,13 +3,17 @@ using namespace std;
 
 int main()
 {
-	int x=4, y=4, z;
+	int x=1, y=4, z;
 
-	if(x>2) {
+/*	if(x>2) {
 		if(y>2) {
 			z=x+y;
 			cout << "z = " << z << endl;
 		}
+	}*/
+	if(x>2 && y>2){
+		z=x+y;
+		cout << "z = " << z << endl;
 	}
 	else
 		cout << "x = " << x << endl;
